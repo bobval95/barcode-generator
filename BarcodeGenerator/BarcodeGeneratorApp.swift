ebd23afa361ea7461cb53741f690bfa7ce2d4be6
Boyan <b.valkanov@gmail.com>
2022-08-23 19:55:47 +0300

    Initial Commit

---
+++ BarcodeGenerator/BarcodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeGeneratorApp.swift
+//  BarcodeGenerator
+//
+//  Created by Boyan Valkanov on 23.08.22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
